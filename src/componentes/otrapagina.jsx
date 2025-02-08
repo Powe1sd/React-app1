d9a3f8931e02107e05272e9582bf13b6d7cd306c
temssl <brayan324554@gmail.com>
2025-02-08 01:17:26 -0500

    first commit

--- src/componentes/otrapagina.jsx
+++ src/componentes/otrapagina.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import "./OtraPagina.css"; // Importa el archivo CSS para estilos específicos
+import "./OtraPagina.css";
 
 const OtraPagina = () => {
+  const [loading, setLoading] = useState(true);
   const productos = [
     {
       id: 1,
       nombre: "Producto 1",
-      precio: 29000, // Precio en pesos colombianos
+      precio: 29000,
       imagen: "https://via.placeholder.com/200",
       descripcion:
         "Descripción del producto 1. Este es un producto fantástico con características increíbles.",
@@ -15,7 +16,7 @@ const OtraPagina = () => {
     {
       id: 2,
       nombre: "Producto 2",
-      precio: 24000, // Precio en pesos colombianos
+      precio: 24000,
       imagen: "https://via.placeholder.com/200",
       descripcion:
         "Descripción del producto 2. Un producto innovador que cambiará tu vida.",
@@ -23,61 +24,55 @@ const OtraPagina = () => {
     {
       id: 3,
       nombre: "Producto 3",
-      precio: 24000, // Precio en pesos colombianos
+      precio: 24000,
       imagen: "https://via.placeholder.com/200",
       descripcion:
         "Descripción del producto 3. La mejor opción para ti. ¡No te arrepentirás!",
     },
-    // Agrega más productos aquí
     {
       id: 4,
       nombre: "Producto 4",
-      precio: 27000, // Precio en pesos colombianos
+      precio: 27000,
       imagen: "https://via.placeholder.com/200",
       descripcion:
         "Descripción del producto 4. La mejor opción para ti. ¡No te arrepentirás!",
     },
-    // Agrega más productos aquí
     {
       id: 5,
       nombre: "Producto 5",
-      precio: 25000, // Precio en pesos colombianos
+      precio: 25000,
       imagen: "https://via.placeholder.com/200",
       descripcion:
         "Descripción del producto 5. La mejor opción para ti. ¡No te arrepentirás!",
     },
-    // Agrega más productos aquí
     {
       id: 6,
       nombre: "Producto 6",
-      precio: 25000, // Precio en pesos colombianos
+      precio: 25000,
       imagen: "https://via.placeholder.com/200",
       descripcion:
         "Descripción del producto 6. La mejor opción para ti. ¡No te arrepentirás!",
     },
-    // Agrega más productos aquí
     {
       id: 7,
       nombre: "Producto 7",
-      precio: 24000, // Precio en pesos colombianos
+      precio: 24000,
       imagen: "https://via.placeholder.com/200",
       descripcion:
         "Descripción del producto 7. La mejor opción para ti. ¡No te arrepentirás!",
     },
-    // Agrega más productos aquí
     {
       id: 8,
       nombre: "Producto 8",
-      precio: 26000, // Precio en pesos colombianos
+      precio: 26000,
       imagen: "https://via.placeholder.com/200",
       descripcion:
         "Descripción del producto 8. La mejor opción para ti. ¡No te arrepentirás!",
     },
-    // Agrega más productos aquí
     {
       id: 9,
       nombre: "Producto 9",
-      precio: 24000, // Precio en pesos colombianos
+      precio: 24000,
       imagen: "https://via.placeholder.com/200",
       descripcion:
         "Descripción del producto 9. La mejor opción para ti. ¡No te arrepentirás!",
@@ -86,38 +81,44 @@ const OtraPagina = () => {
 
   return (
     <div className="container otra-pagina">
-      {/* Clase 'otra-pagina' para estilos específicos */}
-      <h1>Tech</h1>
-      <div className="productos-grid">
-        {/* Grid para mostrar los productos */}
-        {productos.map((producto) => (
-          <div className="producto" key={producto.id}>
-            <img
-              src={producto.imagen}
-              alt={producto.nombre}
-              className="producto-imagen"
-            />
-            <h2 className="producto-nombre">{producto.nombre}</h2>
-            <p className="producto-descripcion">{producto.descripcion}</p>
-            <p className="producto-precio">
-              {new Intl.NumberFormat("es-CO", {
-                style: "currency",
-                currency: "COP",
-                minimumFractionDigits: 0,
-              }).format(producto.precio)}
-            </p>
-            <button className="producto-comprar">Comprar</button>
+      {loading ? (
+        <div className="working-progress">
+          <p>Página en mantenimiento...</p>
+        </div>
+      ) : (
+        <>
+          <h1>Tech</h1>
+          <div className="productos-grid">
+            {productos.map((producto) => (
+              <div className="producto" key={producto.id}>
+                <img
+                  src={producto.imagen}
+                  alt={producto.nombre}
+                  className="producto-imagen"
+                />
+                <h2 className="producto-nombre">{producto.nombre}</h2>
+                <p className="producto-descripcion">{producto.descripcion}</p>
+                <p className="producto-precio">
+                  {new Intl.NumberFormat("es-CO", {
+                    style: "currency",
+                    currency: "COP",
+                    minimumFractionDigits: 0,
+                  }).format(producto.precio)}
+                </p>
+                <button className="producto-comprar">Comprar</button>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          isActive ? "nav-link active" : "nav-link"
-        }
-      >
-        Volver a la página principal
-      </NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Volver a la página principal
+          </NavLink>
+        </>
+      )}
     </div>
   );
 };
---
+++ src/componentes/working-progress.jsx
@@ -0,0 +1,12 @@
+// WorkingProgress.js
+import React from "react";
+
+const WorkingProgress = () => {
+  return (
+    <div className="working-progress">
+      <p>Página en progreso, por favor espera jeje...</p>
+    </div>
+  );
+};
+
+export default WorkingProgress;
